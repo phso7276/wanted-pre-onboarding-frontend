0736b5dda90ff44039a6118c934f74efde9015ce
phso7276 <phso7276@sookmyung.ac.kr>
2023-07-31 11:07:33 +0900

    Feat: todo 리스트 퍼블리싱(#6)
    리스트 페이지에 새로운 TODO를 입력할 수 있는 input과 추가 button 생성

--- src/pages/todo/TodoPage.jsx
+++ src/pages/todo/TodoPage.jsx
@@ -1,9 +1,13 @@
 import useIsLoggedIn from "hooks/useIsLoggedIn";
+import Input from "components/atoms/Input";
+import Button from "components/atoms/button";
 const TodoPage = () => {
   useIsLoggedIn();
   return (
     <div>
-      <div></div>
+      <div>투두리스트</div>
+      <Input data-testid="new-todo-input" />
+      <Button data-testid="new-todo-add-button" text="추가" />
     </div>
   );
 };
