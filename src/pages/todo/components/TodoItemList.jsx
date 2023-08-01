4c546e7f20e115980df3cd8dfd26c711c187cc5b
phso7276 <phso7276@sookmyung.ac.kr>
2023-08-01 18:48:50 +0900

    Feat: Assignment 10 완료(#17)
    투두 리스트의 수정 기능을 구현해주세요
    - 수정 버튼을 누르면 수정모드가 활성화
    수정모드에서는 제출버튼과 취소버튼이 표시
    - 제출버튼을 누르면 수정한 내용을 제출해서 내용이 업데이트 됨
    - 취소버튼을 누르면 수정한 내용을 초기화 수정모드 비활성화

--- src/pages/todo/components/TodoItemList.jsx
+++ src/pages/todo/components/TodoItemList.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "components/organisms/Todoitem";
+import Button from "../../../components/atoms/button";
+import Input from "../../../components/atoms/Input";
 import { updateTodoApi, deleteTodoApi } from "services/apis/todoApi";
 
 const TodoItemList = ({ todo, todoList, setTodoList }) => {
-  const checkboxHandler = () => {
-    updateTodoApi(todo).then((res) => {
-      console.log(res);
-      const data = res.data;
+  const [isEdit, setIsEdit] = useState(false);
+  const [newText, setNewText] = useState(todo.todo);
 
+  const checkboxHandler = (e) => {
+    const newValue = e.target.checked;
+    updateTodoApi(todo.id, todo.todo, newValue).then((res) => {
+      const data = res.data;
+      console.log(data);
       setTodoList(
         todoList.map((item) =>
           item.id === todo.id
             ? { ...item, isCompleted: data.isCompleted }
             : item
+        )
+      );
+    });
+  };
+
+  const onClickUpdateTodo = () => {
+    updateTodoApi(todo.id, newText, todo.isCompleted).then((res) => {
+      const data = res.data;
+      console.log(data);
+      setTodoList(
+        todoList.map((item) =>
+          item.id === todo.id ? { ...item, todo: data.todo } : item
         )
       );
     });
@@ -24,15 +41,72 @@ const TodoItemList = ({ todo, todoList, setTodoList }) => {
       setTodoList(todoList.filter((item) => item.id !== todo.id));
     });
   };
+
   return (
-    <ul>
-      <TodoItem
-        item={todo}
-        key={todo.id}
-        checkboxHandler={checkboxHandler}
-        deleteItem={deleteItem}
-      />
-    </ul>
+    <div>
+      <li>
+        <label>
+          <Input
+            type={"checkbox"}
+            checked={todo.isCompleted}
+            onChange={checkboxHandler}
+            value={todo.id}
+          />
+          {isEdit ? (
+            <>
+              <Input
+                data-testid="modify-input"
+                value={newText}
+                onChange={(e) => {
+                  setNewText(e.target.value);
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <span>{todo.todo}</span>
+            </>
+          )}
+        </label>
+        <div>
+          {isEdit ? (
+            <>
+              <Button
+                data-testid="submit-button"
+                text={"제출"}
+                onClickHandler={() => {
+                  onClickUpdateTodo();
+                  setIsEdit(false);
+                }}
+              />
+              <Button
+                data-testid="cancel-button"
+                text={"취소"}
+                onClickHandler={() => {
+                  setIsEdit(false);
+                  setNewText(todo.todo);
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <Button
+                data-testid="modify-button"
+                text={"수정"}
+                onClickHandler={() => {
+                  setIsEdit(true);
+                }}
+              />
+              <Button
+                data-testid="delete-button"
+                text={"삭제"}
+                onClickHandler={deleteItem}
+              />
+            </>
+          )}
+        </div>
+      </li>
+    </div>
   );
 };
 
