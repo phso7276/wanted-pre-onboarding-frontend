976f86a13183838f450a3b63423d0c600e64eaea
phso7276 <phso7276@sookmyung.ac.kr>
2023-08-05 01:07:56 +0900

    Feat: AppRoute 생성(#28)
    각 페이지마다 index.js 파일 생성 및 경로 정리
    AppRoute.jsx 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import "./App.css";
 import React from "react";
-import { GlobalStyle } from "./styles/globalStyles";
-import { Route, Routes } from "react-router-dom";
+import AppRoute from "pages/AppRoute";
+// import { GlobalStyle } from "./styles/globalStyles";
+// import { Route, Routes } from "react-router-dom";
 
-import SignupPage from "pages/Signup/SiginupPage";
-import SigninPage from "pages/Signin/SigninPage";
-import TodoPage from "pages/todo/TodoPage";
-import NotFound from "components/organisms/NotFound";
-
+// import SignupPage from "pages/Signup/SiginupPage";
+// import SigninPage from "pages/Signin/SigninPage";
+// import TodoPage from "pages/Todo/TodoPage";
+// import NotFound from "./pages/NotFound/NotFound";
+// impor;
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <Routes>
-        <Route path="/signup" element={<SignupPage />}></Route>
-        <Route path="/signin" element={<SigninPage />}></Route>
-
-        <Route path="/todo" element={<TodoPage />}></Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AppRoute />
     </div>
   );
 }
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const NotFoundPage = () => {
+  return <div>Page 404</div>;
+};
+
+export default NotFoundPage;
